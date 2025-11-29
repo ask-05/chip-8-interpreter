34b58ee7f9250f1dfc705ec78ad57b6c3b32eaec
ask-05 <65645141+ask-05@users.noreply.github.com>
2025-11-30 00:06:05 +0500

    added 8XXX, BNNN, CXNN, EXXX and FX0A

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 // #define SDL_MAIN_USE_CALLBACKS 1
-
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <bitset>
-
+#include <cstdlib>
 
 using namespace std;
 
@@ -20,6 +19,7 @@ uint16_t stack[16]; // stack. stores PC addresses to return from subroutines
 uint8_t sp; // stack pointer
 uint8_t delay_timer; // decremented at 60hz until it reaches 0.
 uint8_t sound_timer; // same as delay_timer but it gives off a beeping sound as long as it's not 0.
+uint8_t key[16]; // Keypad state
 
 uint64_t display[32]; // SDL display. 32 lines
 
@@ -46,21 +46,9 @@ uint8_t chip8_fontset[80] = {
 	0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 };
 
-
-// prints out 8 bit binary
-string to_binary_string(unsigned char val) {
-	string binaryString = "";
-
-	for (int i = 7; i >= 0; i--) {
-		if ((val >> i) & 1) {
-			binaryString += '1';
-		}
-		else {
-			binaryString += '0';
-		}
-	}
-
-	return binaryString;
+// Generate random number for CXNN
+int genRandomNum() {
+	return rand() % 256;
 }
 
 // Struct for SDL makes it easy to use
@@ -163,21 +151,54 @@ struct SDLWindow {
 	void SDLLoop() {
 		// Setting initial value
 		pc = 0x200;
+		
  		while (running) {
-			SDL_PollEvent(&event);
-				// Fetch Decode Execute Cycle
-				opcode = (memory[pc] << 8) | memory[pc + 1]; // Fetches next opcode from memory
 
-			if ((opcode >> 12) == 1) { // 1NNN (jump)
+			// Handle events
+			while (SDL_PollEvent(&event)) {
+				if (event.type == SDL_EVENT_QUIT) {
+					running = false;
+				}
+
+				if (event.type == SDL_EVENT_KEY_DOWN || event.type == SDL_EVENT_KEY_UP) {
+					uint8_t key_state = (event.type == SDL_EVENT_KEY_DOWN) ? 1 : 0;
+					switch (event.key.scancode) {
+					case SDL_SCANCODE_1: key[0x1] = key_state; break;
+					case SDL_SCANCODE_2: key[0x2] = key_state; break;
+					case SDL_SCANCODE_3: key[0x3] = key_state; break;
+					case SDL_SCANCODE_4: key[0xC] = key_state; break;
+					case SDL_SCANCODE_Q: key[0x4] = key_state; break;
+					case SDL_SCANCODE_W: key[0x5] = key_state; break;
+					case SDL_SCANCODE_E: key[0x6] = key_state; break;
+					case SDL_SCANCODE_R: key[0xD] = key_state; break;
+					case SDL_SCANCODE_A: key[0x7] = key_state; break;
+					case SDL_SCANCODE_S: key[0x8] = key_state; break;
+					case SDL_SCANCODE_D: key[0x9] = key_state; break;
+					case SDL_SCANCODE_F: key[0xE] = key_state; break;
+					case SDL_SCANCODE_Z: key[0xA] = key_state; break;
+					case SDL_SCANCODE_X: key[0x0] = key_state; break;
+					case SDL_SCANCODE_C: key[0xB] = key_state; break;
+					case SDL_SCANCODE_V: key[0xF] = key_state; break;
+					default: break;
+					}
+				}
+			}
+
+
+			// Fetch Decode Execute Cycle
+			opcode = (memory[pc] << 8) | memory[pc + 1]; // Fetches next opcode from memory
+			//cout << hex << "Opcode: " << opcode << dec << endl;
+			
+			if ((opcode >> 12) == 1) { // 1NNN: jump
 				pc = opcode & 0x0FFF;
 			}
-			else if ((opcode >> 12) == 2) { // 2NNN (call)
+			else if ((opcode >> 12) == 2) { // 2NNN: call
+				stack[sp] = pc; // Stores address of next opcode
 				sp++;
-				stack[sp] = opcode + 2; // Stores address of next opcode
 				pc = opcode & 0x0FFF;
 			}
-			else if ((opcode >> 12) == 3) { // 3XNN Skip if V[X] = NN
-				if ((opcode & 0x00FF) == V[opcode & 0x0F00 >> 8]) {
+			else if ((opcode >> 12) == 3) { // 3XNN: Skip if V[X] = NN
+				if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF)) {
 					incPC();
 					incPC();
 				}
@@ -185,8 +206,8 @@ struct SDLWindow {
 					incPC();
 				}
 			}
-			else if ((opcode >> 12) == 4) { // 4XNN Skip if V[X] != NN
-				if ((opcode & 0x00FF) != V[opcode & 0x0F00 >> 8]) {
+			else if ((opcode >> 12) == 4) { // 4XNN: Skip if V[X] != NN
+				if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF)) {
 					incPC();
 					incPC();
 				}
@@ -194,8 +215,8 @@ struct SDLWindow {
 					incPC();
 				}
 			}
-			else if ((opcode >> 12) == 5) { // 5XY0 Skip if V[X] = V[Y]
-				if ((opcode & 0x0F00 >> 8) == V[opcode & 0x00F0 >> 4]) {
+			else if ((opcode >> 12) == 5) { // 5XY0: Skip if V[X] = V[Y]
+				if (V[(opcode & 0x0F00 >> 8)] == V[(opcode & 0x00F0) >> 4]) {
 					incPC();
 					incPC();
 				}
@@ -203,16 +224,69 @@ struct SDLWindow {
 					incPC();
 				}
 			}
-			else if ((opcode >> 12) == 6) { // 6XNN (set register vx)
+			else if ((opcode >> 12) == 6) { // 6XNN: set register vx
 				V[(opcode & 0x0F00) >> 8] = (uint8_t)opcode;
 				incPC();
 			}
-			else if ((opcode >> 12) == 7) { // 7NNN (add value to register vx)
-				V[(opcode >> 8) ^ 0x0070] += (uint8_t)opcode;
+			else if ((opcode >> 12) == 7) { // 7XNN: add value to register V[X]
+				V[(opcode & 0x0F00) >> 8] += (uint8_t)opcode;
 				incPC();
 			}
+			else if ((opcode >> 12) == 8) { // 8XXX
+				uint16_t tempOpcode = opcode & 0x000F;
+				uint8_t VX = (opcode & 0x0F00) >> 8;
+				uint8_t VY = (opcode & 0x00F0) >> 4;
+				uint16_t sum = V[VX] + V[VY]; // 16 bit number to check overflow for 8XY4
+
+				switch (tempOpcode) {
+				case 0x0: // 8XY0: Set V[X] to V[Y]
+					V[VX] = V[VY];
+					incPC();
+					break;
+				case 0x1: // 8XY1: V[X] is set to the bitwise OR of V[X] and V[Y]
+					V[VX] |= V[VY];
+					incPC();
+					break;
+				case 0x2: // 8XY2: V[X] is set to the bitwise AND of V[X] and V[Y]
+					V[VX] &= V[VY];
+					incPC();
+					break;
+				case 0x3: // 8XY3: V[X] is set to the bitwise XOR of V[X] and V[Y]
+					V[VX] ^= V[VY];
+					incPC();
+					break;
+				case 0x4: // 8XY4: V[X] is set to V[X] + V[Y]
+					V[0xF] = (sum > 255) ? 1 : 0;
+					V[VX] = sum & 0xFF; // V[X] set to lowest 8 bits of 16 bit number
+					incPC();
+					break;
+				case 0x5: // 8XY5: V[X] is set to V[X] - V[Y]
+					V[0xF] = (V[VX] >= V[VY]) ? 1 : 0; // V[F] to 1 if VX > VY
+					V[VX] -= V[VY];
+					incPC();
+					break;
+				case 0x6: // 8XY6: Shift V[X] one bit to the right
+					V[0xF] = V[VX] & 0x1; // Store the LSB of V[X] in V[F]
+					V[VX] >>= 1; // Shift V[X] one bit to the right
+					incPC();
+					break;
+				case 0x7: // 8XY7: V[X] is set to V[Y] - V[X]
+					V[0xF] = (V[VY] >= V[VX]) ? 1 : 0; // V[F] to 1 if VY > VX
+					V[VX] = V[VY] - V[VX];
+					incPC();
+					break;
+				case 0xE: // 8XYE: Shift V[X] one bit to the left
+					V[0xF] = (V[VX] & 0x80) >> 7; // Store the MSB of V[X] in V[F]
+					V[VX] <<= 1; // Shift V[X] one bit to the left
+					incPC();
+					break;
+				default:
+					cout << "Could not find opcode! Nibble: 8 Opcode: " << hex << opcode << dec << endl;
+					break;
+				}
+			}
 			else if ((opcode >> 12) == 9) { // 9XY0 Skip if V[X] != V[Y]
-				if ((opcode & 0x0F00 >> 8) != V[opcode & 0x00F0 >> 4]) {
+				if (V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4]) {
 					incPC();
 					incPC();
 				}
@@ -220,11 +294,18 @@ struct SDLWindow {
 					incPC();
 				}
 			}
-			else if ((opcode >> 12) == 0xA) { //ANNN (set index register I)
+			else if ((opcode >> 12) == 0xA) { //ANNN: set index register I
 				I = (opcode & 0x0FFF);
 				incPC();
 			}
-			else if ((opcode >> 12) == 0xD) { // DXYN (draws to display)
+			else if ((opcode >> 12) == 0xB) { //BNNN: set pc to NNN + V[0]
+				pc = (opcode & 0x0FFF) + V[0];
+			}
+			else if ((opcode >> 12) == 0xC) { //CXNN: sets V[X] to random number & NN
+				V[(opcode & 0x0F00) >> 8] = genRandomNum() & (opcode & 0x00FF);
+				incPC();
+			}
+			else if ((opcode >> 12) == 0xD) { // DXYN: draws to display
 				uint8_t xCoord = V[(opcode & 0x0F00) >> 8] % CHIP8_WIDTH;
 				uint8_t yCoord = V[(opcode & 0x00F0) >> 4] % CHIP8_HEIGHT;
 				int N = opcode & 0x000F; // Height of Sprite
@@ -233,12 +314,10 @@ struct SDLWindow {
 				for (int i = 0; i < N; ++i) {
 					uint8_t spriteByte = memory[I + i];
 					uint8_t drawY = (yCoord + i);
-
 					if (drawY >= CHIP8_HEIGHT) {
 						cout << "broken!" << endl;
 						break;
 					}
-
 					if (xCoord + 8 > CHIP8_WIDTH) {
 						cout << "clipping!" << endl;
 						uint8_t pixelsToClip = (xCoord + 8) - CHIP8_WIDTH;
@@ -246,20 +325,64 @@ struct SDLWindow {
 							spriteByte &= (0xFF << pixelsToClip); // Clears bits that go over screen boundary
 						}
 					}
-
 					int shiftAmount = CHIP8_WIDTH - 8 - xCoord; // Shifts 8 bit sprite into the display row
 					uint64_t spriteVal = ((uint64_t)spriteByte) << shiftAmount;
 					if (display[drawY] & spriteVal) {
 						V[0xF] = 1;
 					}
 					display[drawY] ^= spriteVal;
-
-
-
 				}
-
 				renderScreen();
 				incPC();
+			}
+			else if ((opcode >> 12) == 0xE) { // EXXX
+				uint8_t VX = (opcode & 0x0F00) >> 8;
+				switch (opcode & 0x00FF) {
+				case 0x9E: // EX9E: Skip next instruction if key with the value of Vx is pressed.
+					if (key[V[VX]]) {
+						incPC();
+						incPC();
+					}
+					else {
+						incPC();
+					}
+					break;
+				case 0xA1: // EXA1: Skip next instruction if key with the value of Vx is not pressed.
+					if (!key[V[VX]]) {
+						incPC();
+						incPC();
+					}
+					else {
+						incPC();
+					}
+					break;
+				default:
+					cout << "Could not find opcode! Nibble: E Opcode: " << hex << opcode << dec << endl;
+					break;
+				}
+			}
+			else if ((opcode >> 12) == 0xF) { // FXXX
+				uint8_t VX = (opcode & 0x0F00) >> 8;
+				switch (opcode & 0x00FF) {
+				case 0x0A: // FX0A: Wait for a key press, store the value of the key in Vx.
+				{
+					bool key_pressed = false;
+					for (int i = 0; i < 16; ++i) {
+						if (key[i]) {
+							V[VX] = i;
+							key_pressed = true;
+							break;
+						}
+					}
+					if (key_pressed) {
+						incPC();
+					}
+				}
+				break;
+				default:
+					cout << "Could not find opcode! Nibble: F Opcode: " << hex << opcode << dec << endl;
+					break;
+				}
 			}
 			else {
 				switch (opcode) {
@@ -270,34 +393,16 @@ struct SDLWindow {
 						renderScreen();
 						incPC();
 						break;
-					case 0x00EE:
+					case 0x00EE: // 0x00EE
 						pc = stack[sp];
 						sp--;
 						break;
 					default:
-						cout << "opcode " << opcode << " not found!" << endl;
+						// cout << "opcode " << hex << opcode << dec << " not found!" << endl;
+						break;
 
 				}
 			}
-
-
-			if (event.type == SDL_EVENT_QUIT) {
-				running = false;
-			}
-			else if (event.type == SDL_EVENT_KEY_DOWN) {
-				if (event.key.key >= 49 && event.key.key <= 57) {
-					cout << "KEY NUMBER: " << event.key.key << endl;
-					for (int i = 0; i <= 4; i++) { // This loop draws 0-9 on the screen
-						display[i] = (uint64_t)memory[(0x50 + i) + (5 * (event.key.key - 48))] << 56;
-					}
-					renderScreen();
-					cout << "\n-------------\n";
-				}
-				else if (event.key.key == 48) {
-					opcode = 0x1ABC;
-				}
-			}
-
 		}
 	}
 };
